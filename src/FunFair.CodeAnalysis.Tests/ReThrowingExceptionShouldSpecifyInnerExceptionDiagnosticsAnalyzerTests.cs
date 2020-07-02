41edcefc92c70ad8321b58d53a80498057cb3d23
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-07-02 13:50:16 +0100

    FF-2591 - Re-Throwing Exception as new exception should pass inner exception

---
+++ src/FunFair.CodeAnalysis.Tests/ReThrowingExceptionShouldSpecifyInnerExceptionDiagnosticsAnalyzerTests.cs
@@ -0,0 +1,6 @@
+﻿namespace FunFair.CodeAnalysis.Tests
+{
+    public class ReThrowingExceptionShouldSpecifyInnerExceptionDiagnosticsAnalyzerTests
+    {
+    }
+}
---
+++ src/FunFair.CodeAnalysis/ReThrowingExceptionShouldSpecifyInnerExceptionDiagnosticsAnalyzer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace FunFair.CodeAnalysis
+{
+    /// <summary>
+    ///     Looks for catches which throw a new exception without passing the exception as an inner exception.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class ReThrowingExceptionShouldSpecifyInnerExceptionDiagnosticsAnalyzer : DiagnosticAnalyzer
+    {
+        private const string CATEGORY = "Classes";
+
+        private static readonly DiagnosticDescriptor Rule = RuleHelpers.CreateRule(code: Rules.RuleClassesShouldBeStaticSealedOrAbstract,
+                                                                                   category: CATEGORY,
+                                                                                   title: "Classes should be static, sealed or abstract",
+                                                                                   message: "Classes should be static, sealed or abstract");
+
+        /// <inheritdoc />
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => new[] {Rule}.ToImmutableArray();
+
+        /// <inheritdoc />
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterCompilationStartAction(PerformCheck);
+        }
+
+        private static void PerformCheck(CompilationStartAnalysisContext compilationStartContext)
+        {
+            compilationStartContext.RegisterSyntaxNodeAction(action: MustBeReadOnly, SyntaxKind.ClassDeclaration);
+        }
+
+        private static void MustBeReadOnly(SyntaxNodeAnalysisContext syntaxNodeAnalysisContext)
+        {
+            if (!(syntaxNodeAnalysisContext.Node is ClassDeclarationSyntax classDeclarationSyntax))
+            {
+            }
+
+            // if (!classDeclarationSyntax.Modifiers.Any(IsWhiteListedClassModifier))
+            // {
+            //     syntaxNodeAnalysisContext.ReportDiagnostic(Diagnostic.Create(descriptor: Rule, classDeclarationSyntax.GetLocation()));
+            // }
+        }
+    }
+}
