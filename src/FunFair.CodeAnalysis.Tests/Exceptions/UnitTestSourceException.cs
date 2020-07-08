85bc7eafacbbe0dd7b382126c3cdca1e6d9ec5c3
Amel Salibasic <nukecx@gmail.com>
2020-07-08 04:57:10 +0100

    feat: unit test source exception

---
+++ src/FunFair.CodeAnalysis.Tests/Exceptions/UnitTestSourceException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FunFair.CodeAnalysis.Tests.Exceptions
+{
+    /// <summary>
+    ///     Raised when the verified code is invalid.
+    /// </summary>
+    public sealed class UnitTestSourceException : Exception
+    {
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+
+        // ReSharper disable once UnusedMember.Global
+        public UnitTestSourceException()
+            : this(message: "House not ready")
+        {
+        }
+
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+        /// <param name="message">The message to return.</param>
+        public UnitTestSourceException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        ///     Constructor.
+        /// </summary>
+        /// <param name="message">The message to return.</param>
+        /// <param name="innerException">The inner exception.</param>
+
+        // ReSharper disable once UnusedMember.Global
+        public UnitTestSourceException(string message, Exception innerException)
+            : base(message: message, innerException: innerException)
+        {
+        }
+    }
+}
