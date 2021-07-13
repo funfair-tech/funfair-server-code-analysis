6e120290a60f0ac73f5614179544f3092160e4ac
Vladimir Nesic <vladimir.nesic@funfair.eu>
2021-07-13 05:33:55 +0200

    check if symbol has containing namespace

--- src/FunFair.CodeAnalysis/Helpers/TypeSymbolHelpers.cs
+++ src/FunFair.CodeAnalysis/Helpers/TypeSymbolHelpers.cs
@@ -4,9 +4,19 @@ namespace FunFair.CodeAnalysis.Helpers
 {
     internal static class TypeSymbolHelpers
     {
-        public static string ToFullyQualifiedName(this ITypeSymbol symbol)
+        public static string? ToFullyQualifiedName(this ITypeSymbol symbol)
         {
+            if (symbol.OriginalDefinition.ContainingNamespace == null)
+            {
+                return null;
+            }
+
             string nameSpace = symbol.OriginalDefinition.ContainingNamespace.ToDisplayString();
+
+            if (string.IsNullOrEmpty(nameSpace))
+            {
+                return null;
+            }
 
             return $"{nameSpace}.{symbol.OriginalDefinition.MetadataName}";
         }
