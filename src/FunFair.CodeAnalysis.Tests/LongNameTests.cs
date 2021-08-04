f14b7903c52abb191ab8caf6382015b50211c4cd
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-08-04 15:20:47 +0100

    Added test for too long names

---
+++ src/FunFair.CodeAnalysis.Tests/LongNameTests.cs
@@ -0,0 +1,16 @@
+﻿using FunFair.Test.Common;
+using Xunit;
+
+namespace FunFair.CodeAnalysis.Tests
+{
+    public sealed class LongNameTests : TestBase
+    {
+        [Fact]
+        public void
+            ThisTestNameIsToLongCreateShuftiSessionAsyncShouldReturnAlreadyPendingSessionWhenSessionIsAlreadyPendingAsyncCreateShuftiSessionAsyncShouldReturnAlreadyPendingSessionWhenSessionIsAlreadyPending()
+        {
+            // 171
+            // FunFair.FunWallet.Api.ServiceInterface.Tests.Controllers.KycShuftiControllerTest.CreateShuftiSessionAsyncShouldReturnAlreadyPendingSessionWhenSessionIsAlreadyPendingAsync
+        }
+    }
+}
