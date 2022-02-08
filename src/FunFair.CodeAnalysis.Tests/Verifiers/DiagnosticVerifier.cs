3c4031689bd72b5a0d486cdd1a218b8e151ca4fd
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-02-08 00:06:12 +0000

    [FF-2244] - Code cleanup on FunFair.CodeAnalysis.sln

--- src/FunFair.CodeAnalysis.Tests/Verifiers/DiagnosticVerifier.cs
+++ src/FunFair.CodeAnalysis.Tests/Verifiers/DiagnosticVerifier.cs
@@ -73,7 +73,8 @@ public abstract partial class DiagnosticVerifier : TestBase
                 LinePosition linePosition = diagnostic.Location.GetLineSpan()
                                                       .StartLinePosition;
 
-                builder = builder.Append(provider: CultureInfo.InvariantCulture, $"{resultMethodName}({linePosition.Line + 1}, {linePosition.Character + 1}, {analyzerType.Name}.{rule.Id})");
+                builder = builder.Append(provider: CultureInfo.InvariantCulture,
+                                         $"{resultMethodName}({linePosition.Line + 1}, {linePosition.Character + 1}, {analyzerType.Name}.{rule.Id})");
             }
 
             builder = builder.Append(value: ',')
@@ -118,7 +119,14 @@ public abstract partial class DiagnosticVerifier : TestBase
     {
         DiagnosticAnalyzer diagnostic = AssertReallyNotNull(this.GetCSharpDiagnosticAnalyzer());
 
-        return VerifyDiagnosticsAsync(new[] { source }, references: references, language: LanguageNames.CSharp, analyzer: diagnostic, expected: expected);
+        return VerifyDiagnosticsAsync(new[]
+                                      {
+                                          source
+                                      },
+                                      references: references,
+                                      language: LanguageNames.CSharp,
+                                      analyzer: diagnostic,
+                                      expected: expected);
     }
 
     /// <summary>
@@ -156,7 +164,11 @@ public abstract partial class DiagnosticVerifier : TestBase
     /// <param name="language">The language of the classes represented by the source strings</param>
     /// <param name="analyzer">The analyzer to be run on the source code</param>
     /// <param name="expected">DiagnosticResults that should appear after the analyzer is run on the sources</param>
-    private static async Task VerifyDiagnosticsAsync(string[] sources, MetadataReference[] references, string language, DiagnosticAnalyzer analyzer, params DiagnosticResult[] expected)
+    private static async Task VerifyDiagnosticsAsync(string[] sources,
+                                                     MetadataReference[] references,
+                                                     string language,
+                                                     DiagnosticAnalyzer analyzer,
+                                                     params DiagnosticResult[] expected)
     {
         IReadOnlyList<Diagnostic> diagnostics = await GetSortedDiagnosticsAsync(sources: sources, references: references, language: language, analyzer: analyzer);
 
@@ -186,7 +198,8 @@ public abstract partial class DiagnosticVerifier : TestBase
                 ? FormatDiagnostics(analyzer: analyzer, results.ToArray())
                 : "    NONE.";
 
-            Assert.True(condition: false, $"Mismatch between number of diagnostics returned, expected \"{expectedCount}\" actual \"{actualCount}\"\r\n\r\nDiagnostics:\r\n{diagnosticsOutput}\r\n");
+            Assert.True(condition: false,
+                        $"Mismatch between number of diagnostics returned, expected \"{expectedCount}\" actual \"{actualCount}\"\r\n\r\nDiagnostics:\r\n{diagnosticsOutput}\r\n");
         }
 
         for (int i = 0; i < expectedResults.Length; i++)
@@ -210,7 +223,8 @@ public abstract partial class DiagnosticVerifier : TestBase
             VerifyAdditionalDiagnosticLocations(analyzer: analyzer, actual: actual, expected: expected);
         }
 
-        Assert.True(actual.Id == expected.Id, $"Expected diagnostic id to be \"{expected.Id}\" was \"{actual.Id}\"\r\n\r\nDiagnostic:\r\n    {FormatDiagnostics(analyzer: analyzer, actual)}\r\n");
+        Assert.True(actual.Id == expected.Id,
+                    $"Expected diagnostic id to be \"{expected.Id}\" was \"{actual.Id}\"\r\n\r\nDiagnostic:\r\n    {FormatDiagnostics(analyzer: analyzer, actual)}\r\n");
 
         Assert.True(actual.Severity == expected.Severity,
                     $"Expected diagnostic severity to be \"{expected.Severity}\" was \"{actual.Severity}\"\r\n\r\nDiagnostic:\r\n    {FormatDiagnostics(analyzer: analyzer, actual)}\r\n");
