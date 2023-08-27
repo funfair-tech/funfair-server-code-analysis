1200aa64af1534e0f5030f7a41a1a47872dc0c05
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-08-27 21:59:44 +0100

    Readonly record struct

--- src/FunFair.CodeAnalysis/Helpers/Mapping.cs
+++ src/FunFair.CodeAnalysis/Helpers/Mapping.cs
@@ -1,9 +1,9 @@
-using System;
-using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
 
 namespace FunFair.CodeAnalysis.Helpers;
 
-internal sealed class Mapping : IEquatable<Mapping>
+[DebuggerDisplay(value: "{ClassName}.{MethodName}")]
+internal readonly record struct Mapping
 {
     public Mapping(string methodName, string className)
     {
@@ -16,40 +16,4 @@ internal sealed class Mapping : IEquatable<Mapping>
     private string ClassName { get; }
 
     public string QualifiedName => string.Concat(str0: this.ClassName, str1: ".", str2: this.MethodName);
-
-    public bool Equals(Mapping? other)
-    {
-        if (ReferenceEquals(objA: null, objB: other))
-        {
-            return false;
-        }
-
-        if (ReferenceEquals(this, objB: other))
-        {
-            return true;
-        }
-
-        return this.MethodName == other.MethodName && this.ClassName == other.ClassName;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        return ReferenceEquals(this, objB: obj) || obj is Mapping other && this.Equals(other);
-    }
-
-    [SuppressMessage(category: "Meziantou.Analyzer", checkId: "MA0021:Use String Comparer to compute hash codes", Justification = "Not in net stabdard 2.0")]
-    public override int GetHashCode()
-    {
-        return (this.MethodName.GetHashCode() * 397) ^ this.ClassName.GetHashCode();
-    }
-
-    public static bool operator ==(Mapping? left, Mapping? right)
-    {
-        return Equals(objA: left, objB: right);
-    }
-
-    public static bool operator !=(Mapping? left, Mapping? right)
-    {
-        return !Equals(objA: left, objB: right);
-    }
 }
