77e4d974f6989a2113d5710b1f5189d8c4428427
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 02:27:38 +0000

    Cleanup: Mapping.cs

--- src/FunFair.CodeAnalysis/Helpers/Mapping.cs
+++ src/FunFair.CodeAnalysis/Helpers/Mapping.cs
@@ -15,6 +15,5 @@ internal readonly record struct Mapping
 
     private string ClassName { get; }
 
-    public string QualifiedName =>
-        string.Concat(str0: this.ClassName, str1: ".", str2: this.MethodName);
+    public string QualifiedName => string.Concat(str0: this.ClassName, str1: ".", str2: this.MethodName);
 }
