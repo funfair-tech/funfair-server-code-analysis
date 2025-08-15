3d7a944e8ec869ca3dd5dda89ab5cbecce02d5de
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-08-15 11:11:26 +0100

    Placeholder

---
+++ src/FunFair.CodeAnalysis/ProhibitedEnumMembersAnalyzer.cs
@@ -0,0 +1,88 @@
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using FunFair.CodeAnalysis.Helpers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace FunFair.CodeAnalysis;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class ProhibitedEnumMembersAnalyzer : DiagnosticAnalyzer
+{
+    private static readonly ProhibitedEmumsSpec[] BannedEnums = [
+        Build(ruleId: Rules.RuleDontUseStringComparisonInvariantCulture,
+              title: "Use System.StringComparison.Ordinal instead",
+              message: "Use System.StringComparison.Ordinal instead",
+              sourceEnum: "System.StringComparison", nameof(System.StringComparison.InvariantCulture)),
+        Build(ruleId: Rules.RuleDontUseStringComparisonInvariantCultureIgnoreCase,
+        title: "Use System.StringComparison.OrdinalIgnoreCase instead",
+        message: "Use System.StringComparison.OrdinalIgnoreCase instead",
+        sourceEnum: "System.StringComparison", nameof(System.StringComparison.InvariantCultureIgnoreCase))
+    ];
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = [.. BannedEnums.Select(selector: r => r.Rule)];
+
+    /* TODO ban these fields... probably needs a new analyzer
+  F:System.StringComparison.InvariantCulture;Do you mean Ordinal?
+   F:System.StringComparison.InvariantCultureIgnoreCase;Do you mean OrdinalIgnoreCase?
+ */
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+
+        context.RegisterCompilationStartAction(PerformCheck);
+    }
+
+    private static void PerformCheck(CompilationStartAnalysisContext compilationStartContext)
+    {
+        // Also check constants and assignments to variables
+        compilationStartContext.RegisterSyntaxNodeAction(action: ParameterCannotBeProhibitedEnum, SyntaxKind.Parameter);
+    }
+
+    private static void ParameterCannotBeProhibitedEnum(SyntaxNodeAnalysisContext syntaxNodeAnalysisContext)
+    {
+        // TODO
+        if (syntaxNodeAnalysisContext.Node is ParameterSyntax)
+        {
+            Debug.WriteLine("Parameter");
+        }
+    }
+
+    private static ProhibitedEmumsSpec Build(
+        string ruleId,
+        string title,
+        string message,
+        string sourceEnum,
+        string bannedEnumValue
+    )
+    {
+        return new(
+            ruleId: ruleId,
+            title: title,
+            message: message,
+            sourceEnum: sourceEnum,
+            bannedEnumValue: bannedEnumValue
+        );
+    }
+
+    private readonly record struct ProhibitedEmumsSpec
+    {
+        public ProhibitedEmumsSpec(string ruleId, string title, string message, string sourceEnum, string bannedEnumValue)
+        {
+            this.SourceEnum = sourceEnum;
+            this.BannedEnumValue = bannedEnumValue;
+            this.Rule = RuleHelpers.CreateRule(code: ruleId, category: Categories.IllegalMethodCalls, title: title, message: message);
+        }
+
+        public string SourceEnum { get; }
+
+        public string BannedEnumValue { get; }
+
+        public DiagnosticDescriptor Rule { get; }
+    }
+}
