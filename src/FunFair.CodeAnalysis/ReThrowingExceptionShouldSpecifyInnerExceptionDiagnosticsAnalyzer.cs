c249e37b05182dee6cf0e41e3ea3445bac79c11f
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-16 20:01:18 +0000

    Cleanup: ReThrowingExceptionShouldSpecifyInnerExceptionDiagnosticsAnalyzer.cs

--- src/FunFair.CodeAnalysis/ReThrowingExceptionShouldSpecifyInnerExceptionDiagnosticsAnalyzer.cs
+++ src/FunFair.CodeAnalysis/ReThrowingExceptionShouldSpecifyInnerExceptionDiagnosticsAnalyzer.cs
@@ -13,10 +13,12 @@ namespace FunFair.CodeAnalysis;
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public sealed class ReThrowingExceptionShouldSpecifyInnerExceptionDiagnosticsAnalyzer : DiagnosticAnalyzer
 {
-    private static readonly DiagnosticDescriptor Rule = RuleHelpers.CreateRule(code: Rules.RuleMustPassInterExceptionToExceptionsThrownInCatchBlock,
-                                                                               category: Categories.Exceptions,
-                                                                               title: "Pass an inner exception when thrown from a catch clause",
-                                                                               message: "Provide '{0}' as an inner exception when thrown from catch clauses");
+    private static readonly DiagnosticDescriptor Rule = RuleHelpers.CreateRule(
+        code: Rules.RuleMustPassInterExceptionToExceptionsThrownInCatchBlock,
+        category: Categories.Exceptions,
+        title: "Pass an inner exception when thrown from a catch clause",
+        message: "Provide '{0}' as an inner exception when thrown from catch clauses"
+    );
 
     public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => SupportedDiagnosisList.Build(Rule);
 
@@ -47,50 +49,89 @@ public sealed class ReThrowingExceptionShouldSpecifyInnerExceptionDiagnosticsAna
             return;
         }
 
-        FindErrors(syntaxNodeAnalysisContext: syntaxNodeAnalysisContext, catchClause: catchClause, exceptionVariable: exceptionVariable);
+        FindErrors(
+            syntaxNodeAnalysisContext: syntaxNodeAnalysisContext,
+            catchClause: catchClause,
+            exceptionVariable: exceptionVariable
+        );
     }
 
-    private static void FindErrors(SyntaxNodeAnalysisContext syntaxNodeAnalysisContext, CatchClauseSyntax catchClause, string exceptionVariable)
+    private static void FindErrors(
+        SyntaxNodeAnalysisContext syntaxNodeAnalysisContext,
+        CatchClauseSyntax catchClause,
+        string exceptionVariable
+    )
     {
         // ! argumentList is always not null here
-        catchClause.Block.DescendantNodes()
-                   .Select(ThrowExpressionType)
-                   .RemoveNulls()
-                   .Select(ExpressionWithArgumentList)
-                   .Where(expression => expression.argumentList is not null)
-                   .ForEach(item => CheckArgumentList(item.argumentList!, exceptionVariable: exceptionVariable, syntaxNodeContext: syntaxNodeAnalysisContext, item.expression.GetLocation()));
+        catchClause
+            .Block.DescendantNodes()
+            .Select(ThrowExpressionType)
+            .RemoveNulls()
+            .Select(ExpressionWithArgumentList)
+            .Where(expression => expression.argumentList is not null)
+            .ForEach(item =>
+                CheckArgumentList(
+                    item.argumentList!,
+                    exceptionVariable: exceptionVariable,
+                    syntaxNodeContext: syntaxNodeAnalysisContext,
+                    item.expression.GetLocation()
+                )
+            );
     }
 
-    private static (ExpressionSyntax expression, ArgumentListSyntax? argumentList) ExpressionWithArgumentList(ExpressionSyntax expression)
+    private static (ExpressionSyntax expression, ArgumentListSyntax? argumentList) ExpressionWithArgumentList(
+        ExpressionSyntax expression
+    )
     {
-        return (expression, argumentList: expression switch
-        {
-            ObjectCreationExpressionSyntax objectCreation => objectCreation.ArgumentList,
-            InvocationExpressionSyntax invocation => invocation.ArgumentList,
-            _ => null
-        });
+        return (
+            expression,
+            argumentList: expression switch
+            {
+                ObjectCreationExpressionSyntax objectCreation => objectCreation.ArgumentList,
+                InvocationExpressionSyntax invocation => invocation.ArgumentList,
+                _ => null,
+            }
+        );
     }
 
-    private static void CheckArgumentList(ArgumentListSyntax argumentList, string exceptionVariable, in SyntaxNodeAnalysisContext syntaxNodeContext, Location location)
+    private static void CheckArgumentList(
+        ArgumentListSyntax argumentList,
+        string exceptionVariable,
+        in SyntaxNodeAnalysisContext syntaxNodeContext,
+        Location location
+    )
     {
         if (argumentList.Arguments.Count == 0)
         {
-            ReportDiagnostic(exceptionVariable: exceptionVariable, syntaxNodeContext: syntaxNodeContext, location: location);
+            ReportDiagnostic(
+                exceptionVariable: exceptionVariable,
+                syntaxNodeContext: syntaxNodeContext,
+                location: location
+            );
 
             return;
         }
 
         bool hasExceptionArgument = argumentList.Arguments.Any(argument =>
-                                                                   argument.Expression is IdentifierNameSyntax identifier &&
-                                                                   StringComparer.Ordinal.Equals(x: identifier.Identifier.Text, y: exceptionVariable));
+            argument.Expression is IdentifierNameSyntax identifier
+            && StringComparer.Ordinal.Equals(x: identifier.Identifier.Text, y: exceptionVariable)
+        );
 
         if (!hasExceptionArgument)
         {
-            ReportDiagnostic(exceptionVariable: exceptionVariable, syntaxNodeContext: syntaxNodeContext, location: location);
+            ReportDiagnostic(
+                exceptionVariable: exceptionVariable,
+                syntaxNodeContext: syntaxNodeContext,
+                location: location
+            );
         }
     }
 
-    private static void ReportDiagnostic(string exceptionVariable, in SyntaxNodeAnalysisContext syntaxNodeContext, Location location)
+    private static void ReportDiagnostic(
+        string exceptionVariable,
+        in SyntaxNodeAnalysisContext syntaxNodeContext,
+        Location location
+    )
     {
         syntaxNodeContext.ReportDiagnostic(Diagnostic.Create(descriptor: Rule, location: location, exceptionVariable));
     }
@@ -101,7 +142,7 @@ public sealed class ReThrowingExceptionShouldSpecifyInnerExceptionDiagnosticsAna
         {
             ThrowStatementSyntax throwStatement => throwStatement.Expression,
             ThrowExpressionSyntax throwExpression => throwExpression.Expression,
-            _ => null
+            _ => null,
         };
     }
 }
