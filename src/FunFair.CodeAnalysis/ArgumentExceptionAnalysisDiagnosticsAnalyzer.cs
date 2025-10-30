4ed5e73266372586968a5cedcbc34a53b8a276b8
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-30 17:31:53 +0000

    Removed unused usings

--- src/FunFair.CodeAnalysis/ArgumentExceptionAnalysisDiagnosticsAnalyzer.cs
+++ src/FunFair.CodeAnalysis/ArgumentExceptionAnalysisDiagnosticsAnalyzer.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
